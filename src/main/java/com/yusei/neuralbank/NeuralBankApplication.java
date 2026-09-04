3dff2d72af4150064cac6c5cd21a39551a0a4f78
yusei21 <asmagic111@gmail.com>
2026-09-04 13:25:29 -0300

    feat: add Spring Boot application entrypoint

---
+++ src/main/java/com/yusei/neuralbank/NeuralBankApplication.java
@@ -0,0 +1,12 @@
+package com.yusei.neuralbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuralBankApplication.class, args);
+    }
+}
